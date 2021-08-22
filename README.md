029c1398a991525bbfc91a328e0d3957bbf7841c
Wright <calebogundiya@gmail.com>
2021-08-22 08:39:48 +0100

    States for form fields

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 1. Install the dependencies using ```npm install```
 2. Run ```npm start```
 3. Enter the **block number** and **address** you're query for on the application
+
+## NOTE
--- eth-trans-app/src/App.js
+++ eth-trans-app/src/App.js
@@ -1,33 +1,81 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [from, setFrom] = useState(0x0);
+  const [to, setTo] = useState(0x0);
+  const [date, setDate] = useState(Date.now());
+  const [address, setAddress] = useState(0x0);
+
+  const response = [{
+
+  }]
+
+  function queryChain(e) {
+    e.preventDefault();
+    console.log(from, to, date, address)
+  }
+
+  function handleSetFrom(e) {
+    console.log(e.target.value)
+    let from = e.target.value;
+    setFrom(from);
+  }
+
+  function handleSetTo(e) {
+    console.log(e.target.value)
+    let to = e.target.value;
+    setTo(to);
+  }
+
+  function handleSetAddress(e) {
+    console.log(e.target.value)
+    let address = e.target.value;
+    setAddress(address);
+  }
+
+  function handleSetDate(e) {
+    console.log(e.target.value)
+    let date = e.target.value;
+    setDate(date);
+  }
+
   return (
     <div className="App">
       <h1>Eth Transaction History</h1>
 
       <div>
-        {/* Limit at 1,920,000 to faactor in the DAO fork */}
-        <input type="number" min="1920000" placeholder="Block Number (from)" required />
-        <input type="number" min="1920000" placeholder="Block Number (to)" />
-        <input type="date" placeholder="date of transaction" />
-        <input type="text" placeholder="Address" required />
-        <button>Search</button>
+        <form onSubmit={queryChain}>
+          <input type="number" min="0" placeholder="Block Number (from)" required onChange={handleSetFrom} />
+          <input type="number" min="0" placeholder="Block Number (to)" onChange={handleSetTo} />
+          <input type="text" placeholder="Address" required onChange={handleSetAddress} />
+          <input type="date" placeholder="date of transaction" onChange={handleSetDate} />
+          <button type="submit">Submit</button>
+        </form>
       </div>
 
       <div>
         <table>
           <thead>
-            <th>Tx Hash</th>
-            <th>Tx To</th>
-            <th>Tx From</th>
-            <th></th>
-            <th></th>
-            <th></th>
-            <th></th>
+            <tr>
+              <th>TX Hash</th>
+              <th>TX To</th>
+              <th>TX From</th>
+              <th>TX Value(wei)</th>
+              <th>TX Nonce</th>
+              <th>TX Data(if any)</th>
+              <th>TX Date</th>
+            </tr>
           </thead>
           <tbody>
             <tr>
-
+              <td>0x13ea5D25</td>
+              <td>0x13ea...5D25</td>
+              <td>0x13ea...5D25</td>
+              <td>0x13ea5D25</td>
+              <td>0x13ea5D25</td>
+              <td>0x13ea5D25</td>
+              <td>257 days ago</td>
             </tr>
           </tbody>
         </table>
