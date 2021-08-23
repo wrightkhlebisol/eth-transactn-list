b8c425e2e1ec91eba3f0dfe470f8826f47696e87
Wright <calebogundiya@gmail.com>
2021-08-23 16:00:54 +0100

    endpoints for current block number and transactions per block

--- eth-trans-server/index.js
+++ eth-trans-server/index.js
@@ -1,7 +1,8 @@
 const { ethers } = require("ethers");
 const express = require('express');
-let app = express();
 
+const app = express();
+const port = 3020
 const network = "rinkeby"
 
 
@@ -12,9 +13,53 @@ const provider = new ethers.getDefaultProvider(
     // }
 );
 
-async function getBlockNumber() {
-    let currentBlockNumber = await provider.getBlockNumber();
-    console.log(currentBlockNumber);
-}
+// parse request bodies (req.body)
+app.use(express.urlencoded({ extended: true }))
 
-getBlockNumber();
+app.get('/', async (req, res) => {
+    res.status(200).send('⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓ ⛓⛓⛓⛓');
+})
+
+// GET LATEST BLOCK NUMBER
+// Not extracting to controllers yet since the project structure isnt very large
+app.get('/latestblock', async (req, res) => {
+    try {
+        let currentBlockNumber = await provider.getBlockNumber();
+        res.status(200).send({
+            status: 'success',
+            message: 'block number request successful',
+            body: currentBlockNumber
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(400).send({
+            status: 'failed',
+            message: 'block number request failed',
+            error: error.message
+        })
+    }
+})
+
+// GET BLOCK BY NUMBER
+app.get('/block/:id/transactions', async (req, res) => {
+    try {
+        let blocktransactions = await provider.getBlockWithTransactions(13082128);
+        res.status(200).send({
+            status: 'success',
+            message: 'block transactions request successful',
+            body: blocktransactions
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(400).send({
+            status: 'failed',
+            message: 'block transactions request failed',
+            error: error.message
+        })
+    }
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
