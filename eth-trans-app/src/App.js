3a71467dc8f8e5cf30b880b4582958adfe10a5ea
Wright <calebogundiya@gmail.com>
2021-08-26 09:33:47 +0100

    Display network

--- eth-trans-app/src/App.js
+++ eth-trans-app/src/App.js
@@ -6,52 +6,39 @@ function App() {
   const [from, setFrom] = useState(0x0);
   const [date, setDate] = useState(Date.now());
   const [address, setAddress] = useState(0x0);
-  const [network, setNetwork] = useState('homestead(mainnet)');
+  const [network, setNetwork] = useState('homestead');
   const [responses, setResponses] = useState([]);
   const [statusMessage, setStatusMessage] = useState('No transaction for current selection');
-
-  // useEffect(() => {
-  //   effect
-  //   return () => {
-  //     cleanup
-  //   }
-  // }, [input])
 
   function queryChain(e) {
     e.preventDefault();
     setStatusMessage('...retrieving transactions');
-    fetch(`${server_url}/block/${from}/transactions/${address}`)
+    fetch(`${server_url}/block/${from}/transactions/${address}/network/${network}`)
       .then(res => res.json())
       .then(res => {
-        console.log(res.body)
         setResponses(res.body);
         setStatusMessage(res.message);
 
       }).catch(e => console.error(e));
 
-    console.log(from, date, address, network)
   }
 
   function handleSetFrom(e) {
-    console.log(e.target.value)
     let from = e.target.value;
     setFrom(from);
   }
 
   function handleSetAddress(e) {
-    console.log(e.target.value)
     let address = e.target.value;
     setAddress(address);
   }
 
   function handleSetDate(e) {
-    console.log(e.target.value)
     let date = e.target.value;
     setDate(date);
   }
 
   function handleSetNetwork(e) {
-    console.log(e.target.value);
     let network = e.target.value;
     setNetwork(network);
   }
@@ -76,7 +63,7 @@ function App() {
       </div>
 
       <div>
-        <h5>Network: <span>{network}</span></h5>
+        <h5>Network: <span className="network">{network === 'homestead' ? 'mainnet' : network}</span></h5>
       </div>
 
       <div className="resultBody">
@@ -89,7 +76,7 @@ function App() {
               <th>gasLimit(wei)</th>
               <th>gasPrice(wei)</th>
               <th>Txn Fee(ether)</th>
-              <th>Value(wei)</th>
+              <th>Value(ether)</th>
               <th>Nonce</th>
               <th>Data(if any)</th>
               <th>Block Number</th>
@@ -108,11 +95,11 @@ function App() {
                     <td>{parseInt(response.gasLimit.hex)}</td>
                     <td>{parseInt(response.gasPrice.hex)}</td>
                     <td>{(parseInt(response.gasLimit.hex) * parseInt(response.gasPrice.hex) / 10 ** 18).toFixed(7)}</td>
-                    <td>{parseInt(response.value.hex)}</td>
+                    <td>{(parseInt(response.value.hex) / 10 ** 18).toFixed(2)}</td>
                     <td>{response.nonce}</td>
-                    <td>{`${response.data.substr(0, 12)}...`}</td>
+                    <td>{response.data === '0x' ? response.data : `${response.data.substr(0, 8)}...`}</td>
                     <td>{response.blockNumber}</td>
-                    <td>{responses.timestamp}</td>
+                    <td>{response.timestamp}</td>
                   </tr>
                 )
                 :
